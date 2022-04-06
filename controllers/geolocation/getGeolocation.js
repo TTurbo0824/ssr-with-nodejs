0c757a0b4877c9ca888a222149ecd9cda5500d96
Kyungjoo Ha <429why@gmail.com>
2022-04-06 20:41:02 +0900

    add: API call

--- controllers/geolocation/getGeolocation.js
+++ controllers/geolocation/getGeolocation.js
@@ -1,10 +1,19 @@
+const fetch = require('node-fetch');
+require('dotenv').config();
+
 const Datastore = require('nedb');
 const database = new Datastore('database.db');
 
-module.exports = (request, response) => {
+module.exports = async (request, response) => {
+  const apiKey = process.env.WEATHER_API_KEY;
   const data = request.body;
   const { lat, lon } = data;
   const currentTime = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  const res = await fetch(apiUrl);
+  const json = await res.json();
+  console.log(json);
+
   database.loadDatabase();
 
   const payload = { time: currentTime, latitude: lat, longitude: lon };
