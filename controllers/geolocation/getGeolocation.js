96a2b3b1f4c9a72aea79cc3182cfa13897e8cb76
Kyungjoo Ha <429why@gmail.com>
2022-04-08 15:39:16 +0900

    add: Test code

--- controllers/geolocation/getGeolocation.js
+++ controllers/geolocation/getGeolocation.js
@@ -9,14 +9,34 @@ module.exports = async (request, response) => {
   const data = request.body;
   const { lat, lon } = data;
   const currentTime = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-  const res = await fetch(apiUrl);
-  const json = await res.json();
-  console.log(json);
+  // const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  // const res = await fetch(apiUrl);
+  // const fetchData = await res.json();
+  const fetchData = {
+    main: {
+      temp: 286.72,
+      feels_like: 284.65,
+      temp_min: 282.9,
+      temp_max: 288.87,
+      pressure: 1017,
+      humidity: 20
+    },
+    name: 'Seoul'
+  };
 
+  // console.log(fetchData);
+
+  let temp = fetchData.main.temp - 273.15;
+  temp = temp.toFixed(2);
   database.loadDatabase();
 
-  const payload = { time: currentTime, latitude: lat, longitude: lon };
+  const payload = {
+    time: currentTime,
+    latitude: lat,
+    longitude: lon,
+    city: fetchData.name,
+    temperature: temp
+  };
   database.insert(payload);
 
   response.status(200).json({
