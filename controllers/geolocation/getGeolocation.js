24ec9efc6a93ac844a4937d7b6a746998116b294
Kyungjoo Ha <429why@gmail.com>
2022-04-10 11:41:56 +0900

    modify: Remove test code

--- controllers/geolocation/getGeolocation.js
+++ controllers/geolocation/getGeolocation.js
@@ -9,25 +9,25 @@ module.exports = async (request, response) => {
   const data = request.body;
   const { lat, lon } = data;
   const currentTime = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
-  // const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-  // const res = await fetch(apiUrl);
-  // const fetchData = await res.json();
-  const fetchData = {
-    main: {
-      temp: 286.72,
-      feels_like: 284.65,
-      temp_min: 282.9,
-      temp_max: 288.87,
-      pressure: 1017,
-      humidity: 20
-    },
-    weather: [
-      {
-        main: 'Clear',
-      }
-    ],
-    name: 'Seoul'
-  };
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  const res = await fetch(apiUrl);
+  const fetchData = await res.json();
+  // const fetchData = {
+  //   main: {
+  //     temp: 286.72,
+  //     feels_like: 284.65,
+  //     temp_min: 282.9,
+  //     temp_max: 288.87,
+  //     pressure: 1017,
+  //     humidity: 20
+  //   },
+  //   weather: [
+  //     {
+  //       main: 'Clear'
+  //     }
+  //   ],
+  //   name: 'Seoul'
+  // };
 
   // console.log(fetchData);
 
